0f21f7dd1e864e82ff21b8dd3ee15ed56947c721
haonguyen4993 <nguyen.thi.hao@framgia.com>
2019-07-31 11:40:23 +0700

    implement profile layout

---
+++ resources/js/components/TheHeader.vue
@@ -0,0 +1,61 @@
+<template>
+  <nav class="header navbar navbar-expand-lg navbar-dark bg-dark">
+    <router-link
+      to="/"
+      class="navbar-brand"
+    >
+      Training Php and Vue
+    </router-link>
+    <button
+      class="navbar-toggler"
+      type="button"
+      data-toggle="collapse"
+      data-target="#collapseNav"
+    >
+      <span class="navbar-toggler-icon" />
+    </button>
+
+    <div
+      id="collapseNav"
+      class="collapse navbar-collapse"
+    >
+      <ul class="navbar-nav ml-auto">
+        <li class="nav-item active">
+          <a
+            class="nav-link"
+            href="#"
+          >{{ $t("route.profile") }}</a>
+        </li>
+        <li class="nav-item">
+          <a
+            class="nav-link"
+            href="#"
+          >{{ $t("route.notifications") }}</a>
+        </li>
+        <li class="nav-item">
+          <a
+            class="nav-link"
+            href="#"
+          >{{ $t("route.groups") }}</a>
+        </li>
+        <li class="nav-item">
+          <button class="btn btn-secondary">
+            {{ $t("auth.signOut") }}
+          </button>
+        </li>
+      </ul>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: 'TheHeader',
+};
+</script>
+
+<style lang='scss' scoped>
+.header {
+  margin-bottom: 1.5rem;
+}
+</style>
--- resources/js/store/getters.js
+++ resources/js/store/getters.js
@@ -1,4 +1,6 @@
 const getters = {
   hello: state => state.app.hello,
+  user: state => state.profile.user,
+  groups: state => state.profile.groups,
 };
 export default getters;
